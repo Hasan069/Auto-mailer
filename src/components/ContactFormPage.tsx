74c3037f99926dfa4b9f8d016fa81aaa9d024491
Cyanider <amihasan420@gmail.com>
2024-10-14 21:08:27 +0600

    Day 3

--- src/components/ContactFormPage.tsx
+++ src/components/ContactFormPage.tsx
@@ -3,21 +3,31 @@ import React from "react";
 import { Form } from "react-hook-form";
 import { FormControl, FormField, FormItem, FormMessage } from "./ui/form";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { form } from "@/validation/test";
 
 const ContactFormPage = () => {
   return (
     <div>
       <Form>
-        <form>
+        <form onSubmit={form.handleSubmit(submit)} className="">
           <FormField name={"name"} control={form.control}>
             <FormControl>
               <FormItem>
-                <Input placeholder="Name" />
+                <Input placeholder="Name" {...field} />
                 <FormMessage />
               </FormItem>
             </FormControl>
           </FormField>
-          form
+          <FormField name={"name"} control={form.control}>
+            <FormControl>
+              <FormItem>
+                <Input placeholder="Email" {...field} />
+                <FormMessage />
+              </FormItem>
+            </FormControl>
+          </FormField>
+          <Button type={"submit"}> Submit</Button>
         </form>
       </Form>
     </div>
